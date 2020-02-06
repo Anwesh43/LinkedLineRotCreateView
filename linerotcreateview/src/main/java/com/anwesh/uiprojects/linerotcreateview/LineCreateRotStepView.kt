9715b71ffd38beb7b84da231c388959e78483d2e
Anwesh43 <anweshthecool0@gmail.com>
2020-02-06 10:40:13 +0530

    adding LineCreateRotStepView class

--- linerotcreateview/src/main/java/com/anwesh/uiprojects/linerotcreateview/LineCreateRotStepView.kt
+++ linerotcreateview/src/main/java/com/anwesh/uiprojects/linerotcreateview/LineCreateRotStepView.kt
@@ -55,3 +55,21 @@ fun Canvas.drawLCRSNode(i : Int, scale : Float, paint : Paint) {
     drawCreateRotLines(scale, w, paint)
     restore()
 }
+
+class LineCreateRotStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN ->  {
+
+            }
+        }
+        return true
+    }
+}
