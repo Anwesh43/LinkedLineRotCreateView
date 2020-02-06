958dead6ca9978495603c611e73b641888656ad8
Anwesh43 <anweshthecool0@gmail.com>
2020-02-06 10:30:25 +0530

    adding constants and libraries

--- linerotcreateview/src/main/java/com/anwesh/uiprojects/linerotcreateview/LineCreateRotStepView.kt
+++ linerotcreateview/src/main/java/com/anwesh/uiprojects/linerotcreateview/LineCreateRotStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.linerotcreateview
 /**
  * Created by anweshmishra on 06/02/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.02f / lines
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
