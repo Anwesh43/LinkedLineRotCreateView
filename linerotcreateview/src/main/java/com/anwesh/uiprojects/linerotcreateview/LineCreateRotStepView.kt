71ae279792b3cc6af3292da7c9a840e01b057611
Anwesh43 <anweshthecool0@gmail.com>
2020-02-06 10:53:13 +0530

    adding LineCreateRotStep class

--- linerotcreateview/src/main/java/com/anwesh/uiprojects/linerotcreateview/LineCreateRotStepView.kt
+++ linerotcreateview/src/main/java/com/anwesh/uiprojects/linerotcreateview/LineCreateRotStepView.kt
@@ -162,4 +162,28 @@ class LineCreateRotStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineCreateRotStep(var i : Int) {
+
+        private val root : LCRSNode = LCRSNode(0)
+        private var curr : LCRSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
